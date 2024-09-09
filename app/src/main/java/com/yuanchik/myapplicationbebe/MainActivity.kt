42077814f4417290a34c8e62e97bf0824643591e
Yuanchik <zlovcistomvide@gmail.com>
2024-09-09 10:39:15 +0300

    work2

--- app/src/main/java/com/yuanchik/myapplicationbebe/MainActivity.kt
+++ app/src/main/java/com/yuanchik/myapplicationbebe/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.yuanchik.myapplicationbebe
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.yuanchik.myapplicationbebe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initMenuButtons()
+    }
+
+    private fun initMenuButtons() {
+        binding.buttonMenu!!.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonFavourites!!.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonWItL!!.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonCompilation!!.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonSettings!!.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
         }
     }
 }
