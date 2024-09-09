14df050bfc5660769afb5637f6c031371e43fb72
Yuanchik <zlovcistomvide@gmail.com>
2024-09-09 11:35:44 +0300

    work3

--- app/src/main/java/com/yuanchik/myapplicationbebe/MainActivity.kt
+++ app/src/main/java/com/yuanchik/myapplicationbebe/MainActivity.kt
@@ -24,23 +24,37 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         initMenuButtons()
+
+
     }
 
     private fun initMenuButtons() {
-        binding.buttonMenu!!.setOnClickListener {
-            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        binding.topAppBar?.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
         }
-        binding.buttonFavourites!!.setOnClickListener {
-            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-        }
-        binding.buttonWItL!!.setOnClickListener {
-            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
-        }
-        binding.buttonCompilation!!.setOnClickListener {
-            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-        }
-        binding.buttonSettings!!.setOnClickListener {
-            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        binding.bottomNavigation?.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
